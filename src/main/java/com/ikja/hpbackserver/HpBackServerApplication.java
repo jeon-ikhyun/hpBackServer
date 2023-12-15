3dac38b6ed9a3b7eb884e7fb341ee948a58b7c5e
jeon-ikhyun <ikjaeyo@icloud.com>
2023-12-15 20:41:08 +0900

    Init Commit

---
+++ src/main/java/com/ikja/hpbackserver/HpBackServerApplication.java
@@ -0,0 +1,13 @@
+package com.ikja.hpbackserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpBackServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpBackServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ikja/hpbackserver/HpBackServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikja.hpbackserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpBackServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
